988e7ee5eca995ce6388e2656646e4859797db04
JOmarCuenca <44130542+JOmarCuenca@users.noreply.github.com>
2018-12-18 13:30:41 -0600

    Finished code

--- VLSM.py
+++ VLSM.py
@@ -18,7 +18,9 @@ def sort(arr,pos):
                      pos[y]=temp
         long-=1
 
-
+#similar to the initialization of the subnets
+#Divides the original ip into its cocmponents in binary
+#in string form and deduces its class
 def convert(ipi):
     origin=divide(ipi)
     clas=clase(origin)
@@ -27,6 +29,7 @@ def convert(ipi):
         res+=x
     return res,clas
 
+#Calculates the requiered bits in order for the hosts to work
 def calculateSize(size,use):
     done=False
     res=0
@@ -39,22 +42,33 @@ def calculateSize(size,use):
             res+=1
     return res
 
+#Creates the string for the broadcast of that subnet
 def broad(quantity):
     res=""
     for x in range(len(quantity)):
         res+='1'
     return res
 
+#creates all the different combinations for the subnets
+#IP,hosts,broadcast
 def permut(noTouch,net,id,first,last,broad):
     always=noTouch+net
     ip=transform_bits2(always+id)
     fhost=transform_bits2(always+first)
     lhost=transform_bits2(always+last)
     broadcast=transform_bits2(always+broad)
+    return [ip,fhost,lhost,broadcast]
+
+#creates the mask for the Subnet using the bits dedicated for the hosts
+def mask(host):
     mask=""
-    return [ip,fhost,lhost,broadcast,mask]
+    for x in range(32-host):
+        mask+='1'
+    for x in range(host):
+        mask+='0'
+    return transform_bits2(mask)
 
-
+#module that makes the actual subnetting
 def root(ip,bits,unmut,large):
     unmutable=ip[:unmut]
     hostBit=calculateSize(large,bits)
@@ -66,11 +80,50 @@ def root(ip,bits,unmut,large):
     lhost=binarySum(broadcast,-1)
     result=permut(unmutable,network,ipnet,fhost,lhost,broadcast)
     next=binarySum((unmutable+network+broadcast),1)
+    result.append(mask(hostBit))
+    result.append(netBits)
     return result,next
 
+#exports everything into a .txt for later analysis
+def export(dictionary):
+    writer=open("VLSM.txt","w")
 
-def vlsm(ipi,sub,num):
+    writer.write('\n')
+    arr=dictionary[1]
+    writer.write("The next subnets are for the ip: "+arr[0]+"\n\n")
+
+    for x in range(1,len(dictionary)+1):
+        string="The subnet number "+str(x)+":"
+        writer.write(string+"\n")
+        arr=dictionary[x]
+        string="The available hosts in this subnet are:\t"+str(arr[6])
+        writer.write(string+"\n")
+        string="Sub_ip= "+arr[0]+" \tHosts= "+arr[1]+" - "+arr[2]+" \tBroadcast= "+arr[3]
+        writer.write(string+"\n" )
+        string="The Subnet mask in decimal form is: "+str(arr[5])+"\t, the subnet mask is: "+arr[4]
+        writer.write(string+"\n\n")
+
+
+    writer.close()
+
+#Mastermind for the code
+#Recieves the parameters and then creates the dictionary
+#where everything is saved for later exportation
+def vlsm(ipi,dump):
+    num=[]
+    sub=[]
+    i=0
+    for x in dump:
+        if(not(x in sub)):
+            sub.append(x)
+            num.append(0)
+    for x in sub:
+        for y in dump:
+            if(x==y):
+                num[i]+=1
+        i+=1
     if(len(sub)!=len(num)):
+        print("Error")
         return "Error"
     origin,cl=convert(ipi)
     sort(sub,num)
@@ -81,19 +134,18 @@ def vlsm(ipi,sub,num):
     for x in range(len(sub)):
         for y in range(num[x]):
             result,origin=root(origin,usebits,unmutBits,sub[x])
+            result.append(sub[x])
             dict[cont]=result
             cont+=1
-    for x in dict:
-        print(dict[x])
+    export(dict)
+    print("Done!")
+    return("Done!")
 
 
 
-
-
-
-
-
-ip="10.0.0.0"
-sub=[50,1000,400,2]
-num=[4,1,8,5]
-vlsm(ip,sub,num)
+#default ip for VLSM
+#just change the parameters to subnet other ip
+ip="192.168.254.0"
+sub=[95,70,50,20,2,2]
+#num=[1,1,1,1,2]
+vlsm(ip,sub)
